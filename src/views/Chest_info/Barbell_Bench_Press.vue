ae4359dba2bb3e1f13949ff99d5a5f55a39f79b8
fs-s <joonas.puuding@ametikool.ee>
2022-05-05 02:46:29 +0300

    Barbell

---
+++ src/views/Chest_info/Barbell_Bench_Press.vue
@@ -0,0 +1,45 @@
+
+<!-- Img, exercise name-->
+<template>
+  <section class="text-gray-400 body-font bg-gradient-to-r from-gray-900 to-indigo-900">
+  <div class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Barbell Bench Press</h1>
+      <p class="mb-8 leading-relaxed">Exercise for chest, triceps and front deltoids</p>
+      <div class="flex justify-center">
+      </div>
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded h-auto w-auto" alt="hero" src="https://cdn.shopify.com/s/files/1/0269/5551/3900/files/Barbell-Bench-Press_0316b783-43b2-44f8-8a2b-b177a2cfcbfc_600x600.png?v=1612137800">
+    </div>
+  </div>
+</section>
+
+<!-- description-->
+  <section class="text-gray-400 body-font bg-gradient-to-r from-gray-900 to-indigo-900">
+  <div class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="text-2xl mb-6 text-white font-medium">Exercise execution guide</h1>
+      <h2 class="sm:text-2xl mb-4 font-medium text-white">Starting position</h2>
+      <p class="mb-8 leading-relaxed">Lying on the bench, your feet flat on the floor. Grasp the bar with a grip wider than shoulder width. Your forearms should be perpendicular to the floor.</p>
+      <h2 class="sm:text-2xl mb-4 font-medium text-white">Execution</h2>
+      <p class="mb-8 leading-relaxed">Unhook the bar and slowly lower it to the lower part of your chest. As you contract your pectorals, push the load upwards until your arms are almost straight.</p>
+      <h2 class="sm:text-2xl mb-4 font-medium text-white">During the whole movement:</h2>
+      <ul class="space-y-3 space-t-3" >
+          <li class="ml-2 marker:text-sky-400 list-disc">Keep your elbows on the outside to put maximum stress on your chest and minimum stress on your anterior deltoids and triceps.</li>
+          <li class="ml-2 marker:text-sky-400 list-disc">Keep your shoulders flat against the bench.</li>
+      </ul>
+      <div class="flex">
+      </div>
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-center  items-center text-center1">
+    <h2 class="sm:text-2xl mb-4 font-medium text-white">Equipment required</h2>
+    <p>Barbell, Bench</p>
+    <h2 class="sm:text-2xl mb-4 font-medium text-white">Main muscles</h2>
+    <p>Chest</p>
+    <h2 class="sm:text-xl text-3xl mb-4 font-medium text-white">Secondary muscles</h2>
+    <p>Front deltoids, triceps</p>
+    </div>
+  </div>
+</section>
+</template>
