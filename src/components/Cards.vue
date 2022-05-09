cbb6f1fffd275d07bd98615183c2e2044c128df3
fs-s <joonas.puuding@ametikool.ee>
2022-05-09 10:13:50 +0300

    fixed exercises boxes

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="p-2 lg:p-4">
-        <div class="h-full bg-gray-200 p-6 rounded-lg">
-            <router-link to=""><img :src="data.image_url" class="hb-40 rounded w-full object-cover object-center mb-6" alt="content"></router-link>
+    <div class="p-2 lg:p-4 h-full">
+        <div class="h-full bg-gray-200 min-h-[300px] p-6 rounded-lg">
+            <router-link to=""><img :src="data.image_url" class="rounded w-full object-cover object-center mb-6" alt="content"></router-link>
             <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{{data.heading}}</h2>
             <p class="leading-relaxed text-base">{{data.desc}}</p>
         </div>
