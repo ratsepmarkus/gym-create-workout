3be5e12d3925710ad938c34d664172ee47609f6f
ratsepmarkus <markusratsep11@gmail.com>
2022-05-12 19:06:16 +0300

    Fixed pull requesterror Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,38 +1,38 @@
 <template>
   <footer class="text-gray-400 bg-gray-900 body-font">
-  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-      <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="20" cy="20" r="20" fill="#6366F1"/>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M23.8821 24.7851V19.0466L28.8444 16.1836L33.8061 19.0466V24.7851L28.8425 27.6486L26.3595 26.215L23.8821 24.7851ZM8.96268 31.2795C15.0448 33.079 20.6414 31.3498 25.7266 27.8449L28.8455 29.6461L35.5411 25.7784V18.0531L28.8438 14.1847L22.146 18.0531V21.9868C21.002 22.2024 19.8486 22.3801 18.6912 22.5145C17.3639 22.6685 16.0348 22.7674 14.7116 22.8034C19.6385 18.0573 17.5537 13.8292 15.5972 9.86536L14.7184 8.02748L7.53902 8C6.05711 11.9227 4.80874 15.8933 5.02433 20.1418C5.23566 24.315 6.80237 27.7717 8.96235 31.279L8.96268 31.2795ZM27.5978 21.1921L28.8438 20.4751L30.0905 21.1921V22.6391L28.8438 23.3587L27.5978 22.6374V21.1921ZM26.0671 20.3162V23.5169L28.8438 25.1185L31.6212 23.5152V20.3162L28.8438 18.7116L26.0671 20.3162ZM22.1467 25.7781L24.0586 26.8813C19.9203 29.5585 15.0227 31.1418 10.0865 29.8029C7.97483 26.3071 6.9139 23.2338 6.75329 20.0602C6.59457 16.9249 7.30549 13.6465 8.73902 9.7329L13.6146 9.75186L14.0464 10.6322C15.9069 14.401 17.9033 18.4485 11.836 22.9697L12.324 24.5285C14.5132 24.5976 16.7141 24.4906 18.8899 24.2378C19.9808 24.1108 21.0686 23.947 22.1481 23.7503L22.1479 25.7783L22.1467 25.7781Z" fill="white"/>
-      </svg>
-      <router-link to="/" class="ml-3 text-xl tracking-wider">FLIPPIN'GYM</router-link>
-    </a>
-    <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2022 FLIPPIN'GYM </p>
-    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a class="text-gray-400">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
+        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="20" cy="20" r="20" fill="#6366F1"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M23.8821 24.7851V19.0466L28.8444 16.1836L33.8061 19.0466V24.7851L28.8425 27.6486L26.3595 26.215L23.8821 24.7851ZM8.96268 31.2795C15.0448 33.079 20.6414 31.3498 25.7266 27.8449L28.8455 29.6461L35.5411 25.7784V18.0531L28.8438 14.1847L22.146 18.0531V21.9868C21.002 22.2024 19.8486 22.3801 18.6912 22.5145C17.3639 22.6685 16.0348 22.7674 14.7116 22.8034C19.6385 18.0573 17.5537 13.8292 15.5972 9.86536L14.7184 8.02748L7.53902 8C6.05711 11.9227 4.80874 15.8933 5.02433 20.1418C5.23566 24.315 6.80237 27.7717 8.96235 31.279L8.96268 31.2795ZM27.5978 21.1921L28.8438 20.4751L30.0905 21.1921V22.6391L28.8438 23.3587L27.5978 22.6374V21.1921ZM26.0671 20.3162V23.5169L28.8438 25.1185L31.6212 23.5152V20.3162L28.8438 18.7116L26.0671 20.3162ZM22.1467 25.7781L24.0586 26.8813C19.9203 29.5585 15.0227 31.1418 10.0865 29.8029C7.97483 26.3071 6.9139 23.2338 6.75329 20.0602C6.59457 16.9249 7.30549 13.6465 8.73902 9.7329L13.6146 9.75186L14.0464 10.6322C15.9069 14.401 17.9033 18.4485 11.836 22.9697L12.324 24.5285C14.5132 24.5976 16.7141 24.4906 18.8899 24.2378C19.9808 24.1108 21.0686 23.947 22.1481 23.7503L22.1479 25.7783L22.1467 25.7781Z" fill="white"/>
         </svg>
+        <span class="ml-3 text-xl tracking-wider">FLIPPIN'GYM</span>
       </a>
-      <a class="ml-3 text-gray-400">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        </svg>
-      </a>
-      <a class="ml-3 text-gray-400">
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg>
-      </a>
-      <a class="ml-3 text-gray-400">
-        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
-      </a>
-    </span>
-  </div>
-</footer>
+      <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2022 FLIPPIN'GYM </p>
+      <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <a class="text-gray-400">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-400">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-400">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-400">
+          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+      </span>
+    </div>
+  </footer>
 </template>
